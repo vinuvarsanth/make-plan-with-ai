46f5d4dc05e5e1e1f7c87003ca52d91b5f468f03
vinuvarsanth <vinuvarsanth01@gmail.com>
2024-08-26 16:50:30 +0530

    Search api Added

--- app/create-trip/search-place.jsx
+++ app/create-trip/search-place.jsx
@@ -1,11 +1,13 @@
 import { useNavigation } from "expo-router";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { View } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { Colors } from "../../constants/Colors";
+import { CreateTripContext } from "./../../context/CreateTripContext";
 
 export default function SearchPlace() {
   const navigation = useNavigation();
+  const { tripData, setTripData } = useContext(CreateTripContext);
 
   useEffect(() => {
     navigation.setOptions({
@@ -15,6 +17,10 @@ export default function SearchPlace() {
     });
   }, [navigation]); // Adding navigation to dependency array for best practices
 
+    useEffect(()=>{
+        console.log(tripData)
+    }),[tripData]
+
   return (
     <View
       style={{
@@ -22,10 +28,10 @@ export default function SearchPlace() {
         paddingTop: 75,
         backgroundColor: Colors.WHITE,
         height: "100%",
-      }}
-    >
+      }}>
+        
       <GooglePlacesAutocomplete
-        placeholder="Search"
+        placeholder="Search Place"
         fetchDetails={true}
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
@@ -33,10 +39,25 @@ export default function SearchPlace() {
           console.log(details?.geometry?.location); // Logs the location geometry (latitude and longitude)
           console.log(details?.photos[0]?.photo_reference); // Logs the photo reference of the first photo
           console.log(details?.url); // Logs the URL of the place
+          setTripData({
+            locationInfo: {
+              name: data.description,
+              coordinates: details?.geometry.location,
+              photoRef: details?.photos[0]?.photo_reference,
+              url: details?.url,
+            }
+          });
         }}
         query={{
           key: process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY, // Replace 'YOUR_API_KEY' with your actual Google Places API key
           language: "en",
+        }}
+        styles={{
+            textInputContainer:{
+                borderWidth:1,
+                borderRadius:5,
+                marginTop:20
+            }
         }}
       />
     </View>
