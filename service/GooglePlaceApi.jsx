a8f2d15a33d31e518f6f2dd6018a451d94dc5dd4
vinuvarsanth <vinuvarsanth01@gmail.com>
2024-09-17 13:35:17 +0530

    Deleted API file which is not in use

--- service/GooglePlaceApi.jsx
+++
@@ -1,8 +0,0 @@
-export const GetPhotoRef =async(placeName)=>{
-    const resp=await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json'+
-  '?query='+placeName+
-  '&key='+process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY);
-  const result = await resp.json();
-  console.log(result);
-  return result;
-}
